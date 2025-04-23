04b6cd4888d2db344229bccbc683f3c1b8fe3bfa
omkargirkar <omkar.girkar18@gmail.com>
2025-04-24 00:03:12 +0530

    expense files added

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-function handleFormSubmit(event) {
-  event.preventDefault();
+function handleAddExpense(event) {
+    event.preventDefault();
+    const amount = document.getElementById('amount').value;
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
 
-  const username = event.target.username.value;
-  const email = event.target.email.value;
-  const password = event.target.password.value;
+    const expense = { amount, description, category };
 
-  const user = { username, email, password };
-
-  fetch("/user/signup", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user)
-  })
-    .then(res => res.json())
-    .then(data => {
-      alert(data.message || 'Signup successful!');
-      event.target.reset();
+    fetch('/expense/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(expense)
     })
-    .catch(err => {
-      console.error(err);
-      alert('Something went wrong. Please try again.');
-    });
+        .then(res => res.json())
+        .then(data => displayOnScreen(data))
+        .catch(err => console.log(err));
+
+    event.target.reset();
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("/expense/get")
+      .then(res => res.json())
+      .then(data => {
+        data.forEach(todo => displayOnScreen(todo));
+      })
+      .catch(err => console.log(err));
+  })
+  
+  function displayOnScreen(obj) {
+      const parentElement = document.getElementById('expenseList');
+      const childElement = document.createElement('li');
+      childElement.id = obj.description;
+      childElement.textContent = `${obj.amount} - ${obj.category} - ${obj.description}`;
+  
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete Expense';
+  
+      childElement.appendChild(deleteButton);
+
+      deleteButton.addEventListener("click", ()=>{
+        fetch(`/expense/delete/${obj.id}`,{
+            method:"DELETE"
+        })
+        .then(()=>parentElement.removeChild(childElement))
+        .catch(err=>console.log(err));
+      });
+
+      parentElement.appendChild(childElement);
+  }

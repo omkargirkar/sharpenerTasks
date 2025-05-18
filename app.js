89b9597e55f169bd138d9ebfb2fa1b18ab5027e7
omkargirkar <omkar.girkar18@gmail.com>
2025-05-19 01:06:50 +0530

    typescript code added

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const express = require('express');
-const path = require('path');
-const userRoutes = require('./routes/userRoutes');
-
-const app = express();
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/user', userRoutes);
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
+var numResults = [];
+var textResults = [];
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
+}
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    numResults.push(result);
+    var stringResult = add(num1, num2);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
+});
+var myPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('It worked');
+    }, 1000);
+});
+myPromise.then(function (result) {
+    console.log(result.split('w'));
 });
